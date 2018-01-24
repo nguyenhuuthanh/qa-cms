e25e65f0bee2d515f23200d38babd763b0f9152b
Toan Nguyen <nta.toan@gmail.com>
2018-01-24 22:50:40 +0700

    Add file loader and url loader

--- webpack.config.js
+++ webpack.config.js
@@ -28,40 +28,57 @@ module.exports = () => ({
           }
         ],
         include: srcPath
-      },
-      {
+      }, {
         test: /\.(js|jsx)$/,
         include: srcPath,
         loader: require.resolve("babel-loader"),
         options: {
           cacheDirectory: true
         }
-      },
-      {
+      }, {
         test: /\.s[ac]ss$/,
         use: [
           {
             loader: "style-loader"
-          },
-          {
+          }, {
             loader: "css-loader",
             options: {
               modules: true,
               localIdentName: "[path][name]__[hash:base64:5]__[local]",
               sourceMap: true
             }
-          },
-          {
+          }, {
             loader: "resolve-url-loader",
             options: {
               sourceMap: true
             }
-          },
-          {
+          }, {
             loader: "sass-loader",
             options: {
               outputStyle: "expanded",
               sourceMap: true
+            }
+          }
+        ]
+      }, {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10000,
+              mimetype: 'application/font-woff'
+            }
+          }
+        ]
+      }, {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              limit: 10000,
+              mimetype: 'application/font-woff'
             }
           }
         ]
